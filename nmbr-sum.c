4f081b27f1dd2c4761f9ff0f4b252527d0e91859
jaorduz <jaorduz@ciencias.unam.mx>
2017-07-10 19:44:17 -0500

    Create nmbr-sum.c
    This file sum a pair of numbers

---
+++ nmbr-sum.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int x,y,z;
+
+int main(){
+
+printf("Dame el primer numero: ");
+
+scanf("%d",&x);
+
+printf("\n");
+
+printf("Dame el segundo numero: ");
+
+scanf("%d",&y);
+
+z=x+y;
+printf("\n\n El resultado de la suma es :%d\n",z);
+return 0;
+}
