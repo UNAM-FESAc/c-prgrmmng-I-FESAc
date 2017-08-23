eb08424d57ba813e013553eabf10a27a0372ca89
J. A. Orduz-Ducuara <jaorduz@ciencias.unam.mx>
2017-08-23 17:38:59 -0500

    Create data_cte-smblc.c

---
+++ data_cte-smblc.c
@@ -0,0 +1,22 @@
+/* Ejemplo de una constante simbolica
+
+#define NOMBRE texto
+*/
+
+#include <stdio.h>
+
+#define RADIO 2.0;
+
+int main()
+{
+
+float r;
+
+r= RADIO;
+
+printf("RADIO = %f", r);
+
+printf("\n");
+
+return 0;
+}
