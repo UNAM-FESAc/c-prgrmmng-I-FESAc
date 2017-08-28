ddc6a2c4d39807e982e463cc11d3056b9007c42c
J. A. Orduz-Ducuara <jaorduz@ciencias.unam.mx>
2017-08-28 08:45:24 -0500

    Update and rename data_cte-smblc.c to char-paragraph.c
    Example: multi-char

---
+++ char-paragraph.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main ( )
+{
+char nombre[30] ;
+
+printf ("Te muestro como puedes escribir una serie lineas.\n");
+printf("Como te gusta que te llamen?");
+scanf("%8s", nombre);
+printf ("\nMucho gusto %8s.", nombre);
+printf ("\nDiviertete mientras aprendes C.\n");
+printf("Lograras mucho.\n");
+}
