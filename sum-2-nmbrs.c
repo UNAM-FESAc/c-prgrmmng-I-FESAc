51ab59e382add83d9ad359f99627b738d8fa0f7a
J. A. Orduz-Ducuara <jaorduz@ciencias.unam.mx>
2017-08-29 17:50:06 -0500

    To-sum-2-numbers
    This file contains basic information on modular programming.

---
+++ sum-2-nmbrs.c
@@ -0,0 +1,18 @@
+/*
+Las siguientes lineas contienen un programa para realizar la suma de dos 
+numeros.
+Analicemos cada linea.
+
+*/
+
+#include<stdio.h>
+
+int main(){
+int c;
+int a=1;
+int b=2;
+c= a+b;
+
+printf("la suma =%i\n", c);
+return 0;
+}
