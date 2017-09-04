94305598a7e04a094bf2b6fa15271a910601a55f
J. A. Orduz-Ducuara <jaorduz@ciencias.unam.mx>
2017-09-04 08:54:37 -0500

    Create 10puts-gets.c
    Puts function

---
+++ 10puts-gets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/*
+La funcion puts coloca una cadena de caracteres en 
+la terminal y da un salto de linea
+ */
+
+main()
+{
+  char nombre[1];
+  printf("dame tu nombre ", nombre);
+  scanf("%s", &nombre);
+  printf("Bienvenido %s a la clase...",nombre);
+  puts("de programación en ");
+  puts("lenguaje C");
+  /*
+La funcion gets toma una entrada de caracteres de la 
+entrada. Esta funcion permite introducir incluso espacios.
+   */
+
+  gets()
+
+
+}
