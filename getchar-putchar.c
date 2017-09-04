c59984b90f995f4622dbf6f11011879a31b0e357
J. A. Orduz-Ducuara <jaorduz@ciencias.unam.mx>
2017-09-04 08:53:07 -0500

    Update and rename char-int-variables.c to getchar-putchar.c
    Getchar and putchar functions

---
+++ getchar-putchar.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/*
+En este ejercicio se introducen las funciones principales de entrada y salida 
+de caracteres.
+ */
+
+
+/*  Ejercicios en clase 
+- Cambia int por void y observa lo que sucede.
+- Omite definir el tipo de la funcion y observa que sucede
+*** Cuando puedas usar la terminal usa: 
+ gcc -Wall nombredelarchivo.c -o nombredelarchivo
+ y analia lo que sucede
+*/
+
+int main()
+{
+  //La funcion getchar no devuelve nada hasta presionar enter
+  getchar();
+  // la funcion putchar escribe un unico caracter en la salida estandar
+	putchar('h');
+	putchar('o');
+	putchar('l');
+	putchar('a');
+	putchar(32); // codigo ASCII para el espacio 
+	putchar('c');
+	putchar('l');
+	putchar('a');
+	putchar('s');
+	putchar('e');
+
+	putchar('\n');
+  getchar();
+  return 0;
+}
