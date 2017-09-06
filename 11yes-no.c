33cad5e1554902c1883990be56d0a5800dd06210
J. A. Orduz-Ducuara <jaorduz@ciencias.unam.mx>
2017-09-05 21:32:53 -0500

    Answers Yes or Not
    This is a file to get output characters

---
+++ 11yes-no.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+//#include <stdlib.h>
+
+int main(void)
+{
+  char c;
+           
+  printf("Introduzca un carácter:");
+  scanf("%c",&c);
+   
+  if (c=='s' || c=='n' ){
+    printf("Es correcto\n");
+  }
+  else
+    {
+      printf("Es incorrecto\n");
+    }
+   
+  //  system("PAUSE");      
+  return 0;
+}
