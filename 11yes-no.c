82c854ccc29af8e23a2136657cb4dfe349813233
J. A. Orduz-Ducuara <jaorduz@ciencias.unam.mx>
2017-09-11 09:03:36 -0500

    Add files via upload
    Yes, no o si

--- 11yes-no.c
+++ 11yes-no.c
@@ -1,24 +1,19 @@
 #include <stdio.h>
-/*
-En este archivo encuentras el uso del operador || (significa "o").
-Debes crear el pseudocódigo y extender erchivo para que las respuestas 
-posibles sea s, n, y.
-Fuente: http://programavideojuegos.blogspot.mx/
-*/
+//#include <stdlib.h>
 
-int main()
+int main(void)
 {
-  char c;
+  char cadena;
            
   printf("Introduzca un carácter:");
-  scanf("%c",&c);
-   
-  if (c=='s' || c=='n' ){
+  scanf("%s",&cadena);
+  if (cadena=='y'|| cadena=='n'||cadena=='s'){
     printf("Es correcto\n");
   }
   else
     {
       printf("Es incorrecto\n");
     }
-     return 0;
+   
+  return 0;
 }
