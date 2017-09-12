4a4d9f2efdd19e64347d75533fac3f5893fc2d36
J. A. Orduz-Ducuara <jaorduz@ciencias.unam.mx>
2017-09-12 16:46:16 -0500

    asignacion

---
+++ 14asignacion-op.c
@@ -0,0 +1,33 @@
+// Como funcionan los operadores de asignacion
+// Aprenderemos como colocar datos en notacion 
+// cientifica en la salida.
+
+
+#include <stdio.h>
+int main()
+{
+    float a = -2.5;
+    float c;
+    int d;
+
+    c = a;
+    printf("c = %.2f \n", c);
+
+// c = c+a. En esta linea se realiza la suma
+    c += a; 
+    printf("c = %.2f \n", c);
+
+// c = c-a. En esta linea se realiza la resta.
+    c -= a; 
+    printf("c = %.2f \n", c);
+
+// c = c*a. En esta linea se realiza el producto
+    c *= a; 
+    printf("c = %.2f \n", c);
+
+// c = c/a. En esta linea se realiza la division
+    c /= a; 
+    printf("c = %2.1e \n", c);
+
+    return 0;
+}
