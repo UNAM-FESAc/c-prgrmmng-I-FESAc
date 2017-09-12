ad1c4d514721614929503633fe8271731940542c
J. A. Orduz-Ducuara <jaorduz@ciencias.unam.mx>
2017-09-12 12:22:22 -0500

    Update 08puts-gets.c

--- 08puts-gets.c
+++ 08puts-gets.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 /*
 La funcion puts coloca una cadena de caracteres en 
-la terminal y da un salto de linea
+la terminal y da un salto de linea. 
+Note que scanf en este formato reconoce cadenas de 
+palabras y la terminacion es un espacio 
+o un fin de linea.
  */
 
-main()
+void main()
 {
   char nombre[1];
-  printf("dame tu nombre ", nombre);
-  scanf("%s", &nombre);
-  printf("Bienvenido %s a la clase...",nombre);
+  printf("dame tu nombre ");
+  scanf("%5s", nombre);
+  printf("Bienvenido %5s a la clase...", nombre);
   puts("de programación en ");
   puts("lenguaje C");
-  /*
-La funcion gets toma una entrada de caracteres de la 
-entrada. Esta funcion permite introducir incluso espacios.
-   */
-
-  gets()
-
 
 }
