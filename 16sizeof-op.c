dbdeeb0220a644276999854f90a19dc78cb43169
J. A. Orduz-Ducuara <jaorduz@ciencias.unam.mx>
2017-09-13 12:37:59 -0500

    Add files via upload

---
+++ 16sizeof-op.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+
+printf("Doble: %d\n", sizeof (double));
+printf("Entero: %d\n", sizeof (int));
+printf("flotante: %d\n", sizeof (float));
+printf("Caracter: %d\n", sizeof (char));
+
+return 0;
+}
