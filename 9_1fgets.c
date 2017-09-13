c2cc7b75eb04712273472f893d953895885431ca
J. A. Orduz-Ducuara <jaorduz@ciencias.unam.mx>
2017-09-13 08:59:35 -0500

    Add files via upload

---
+++ 9_1fgets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+//#include <string.h>
+/*
+La funcion fgets toma una entrada de caracteres de la 
+entrada. 
+En este ejemplo, cadena es el nombre del arreglo de caracteres, 
+sizeof cadena es la cantidad de texto en la entrada más uno; 
+y stdin es el nombre del dispositivo estandar de entrada.
+*/
+
+void main()
+{
+  char cadena[5];
+
+  puts("Escriba un texto:");
+  fgets(cadena, sizeof cadena, stdin);
+  puts("El texto escrito es:");
+  puts(cadena);
+}
+
