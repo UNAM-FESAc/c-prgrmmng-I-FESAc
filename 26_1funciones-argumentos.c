5aa22323ac1eaa9899df2f049909d5a4cd624145
J. A. Orduz-Ducuara <jaorduz@ciencias.unam.mx>
2017-10-17 12:55:29 -0500

    Add files via upload

---
+++ 26_1funciones-argumentos.c
@@ -0,0 +1,33 @@
+/*
+Veamos un ejemplo de un macro con argumentos de varias lineas
+
+
+Calculemos el volumen de un cono.
+*/
+
+
+
+#include <stdio.h>
+#define PI 3.1415
+#define VOLCONO(radio, altura) ((PI*(radio*radio)\
+				*altura)/3.0)
+
+int main(){
+float radio, altura, volumen;
+
+printf("\nIntroduzca el radio del cono [cm]: ");
+scanf("%f", &radio);
+
+printf("\nIntroduzca la altura del cono [cm]: ");
+scanf("%f", &altura);
+
+volumen=VOLCONO(radio, altura);
+
+printf("\nEl volumen del cono es: %.2f cm3\n\v", volumen);
+
+return 0;
+}
+
+
+
+
