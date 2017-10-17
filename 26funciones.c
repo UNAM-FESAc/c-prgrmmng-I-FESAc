65843a1fa33d97d4b801e52cc3392aeea844ef82
J. A. Orduz-Ducuara <jaorduz@ciencias.unam.mx>
2017-10-17 12:56:19 -0500

    Update 26funciones.c

--- 26funciones.c
+++ 26funciones.c
@@ -1,39 +1,4 @@
-#include <stdio.h>
-/*
-Ejercicio debes realizar un programa que use funciones para 
-obtener la media aritmetica.
-*/
 
-
-
-int max(float x, float y){
-if(x < y){
-	return y;
-}else{
-	return x;
-}
-
-}
-
-
-
-
-
-
-
-int main(){
-
-int m, n;
-
-
-do{
-scanf("%d %d", &m, &n);
-printf("maximo valor entre %d, %d es %d\n",m, n, max(m,n));
-}while(m!=0);
-
-
-return 0;
-}
 
 
 
