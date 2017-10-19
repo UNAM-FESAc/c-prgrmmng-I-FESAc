137159097591d4a25565c65a8c4ee813477f3fd7
J. A. Orduz-Ducuara <jaorduz@ciencias.unam.mx>
2017-10-19 13:54:16 -0500

    Add files via upload

---
+++ 27_2funciones-bibliote.c
@@ -0,0 +1,25 @@
+/*
+Este programa recibe un caracter no numerico 
+indefinidamente. El bucle termina al introducir un 
+caracter numerico
+*/
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+
+int i;
+
+
+for (i=1; i<=4; i++){
+printf("%d\n", rand());
+}
+
+return 0;
+}
+
+
+
+
