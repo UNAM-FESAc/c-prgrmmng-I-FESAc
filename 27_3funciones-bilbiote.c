bca85a3d5a14d48310f5bd532638c9f3739e3528
J. A. Orduz-Ducuara <jaorduz@ciencias.unam.mx>
2017-10-25 09:00:54 -0500

    Add files via upload

---
+++ 27_3funciones-bilbiote.c
@@ -0,0 +1,27 @@
+/*
+*/
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+float rr,ss;
+
+
+
+printf("un valor %f\n", rand());
+
+
+srand(100);
+
+rr = rand();
+printf("otro valor %f\n", rr);
+
+return 0;
+}
+
+
+
+
