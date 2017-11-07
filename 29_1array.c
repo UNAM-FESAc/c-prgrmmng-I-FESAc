dda3a7c72e61985c7b811f79e844406d2b8edb3d
J. A. Orduz-Ducuara <jaorduz@ciencias.unam.mx>
2017-11-07 14:47:35 -0600

    Update 29_1array.c

--- 29_1array.c
+++ 29_1array.c
@@ -1,5 +1,6 @@
 /*
-Programa para implementar un arreglo solicitando datos por terminal.
+Ahora veamos una forma de implementar vectores con float.
+Compare con el anterior archivo (29_1array.c).
 */
 
 
@@ -12,7 +13,7 @@ float a[2];
 printf("primer ");
 scanf("%f", &a[0]);
 
-printf("primer ");
+printf("segundo");
 scanf("%f", &a[1]);
 
 printf("Tenemos que a[%3.1f %3.1f]\n", a[0], a[1]);
@@ -20,4 +21,3 @@ printf("Tenemos que a[%3.1f %3.1f]\n", a[0], a[1]);
 
 return 0;
 }
-
