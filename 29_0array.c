982000a461289fed6eeb248108e53529a851116c
J. A. Orduz-Ducuara <jaorduz@ciencias.unam.mx>
2017-11-07 14:46:09 -0600

    Update 29_0array.c

--- 29_0array.c
+++ 29_0array.c
@@ -1,17 +1,50 @@
-/*
-Programa para implementar un arreglo
-*/
-
-
 #include <stdio.h>
 
-int main(){
+void main ()
+{
+char nombre[10];
 
-float a[2]={1, 2};
+int a;
 
 
-printf("Tenemos que a[%3.1f %3.1f]\n", a[0], a[1]);
+printf("Como te gusta que te llamen (sin espacios)? ");
+scanf("%11s", nombre);
 
-return 0;
+
+getchar();
+
+printf ("\nCual caracter quieres imprimir \n");
+scanf("%i", &a);
+
+printf ("\nimprime un caracter %c\n", nombre[a]);
+
+printf ("\nMucho gusto %11s.", nombre);
+printf ("\nDiviertete mientras aprendes C.\n");
+printf("Lograras mucho.\n");
+
 }
 
+
+/*
+Puedes reemplazar 
+getchar();
+
+por 
+while ( getchar() != '\n');
+
+o
+
+system("pause");
+
+en Linux o Windows.
+
+
+******************************
+
+Ejercicio en clase:
+El estudiante debe hacer su programa explore toda la cadena dada y 
+que le busque algun caracter o que le diga que no esta en la cadena.
+
+******************************
+
+*/
