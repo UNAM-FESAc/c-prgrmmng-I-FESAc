a94eb51179752e7185c2ed73ca5bd8d58fb9bdf3
J. A. Orduz-Ducuara <jaorduz@ciencias.unam.mx>
2018-02-01 12:12:05 -0600

    Update 29_4array.c

--- 29_4array.c
+++ 29_4array.c
@@ -8,22 +8,27 @@ TIPO_de_DATO NOMBRE_ARREGLO[Num_filas][Num_columnas]
 
 Los indices estan dados de la siguiente forma:
 
-TIPO_de_DATO NOMBRE_ARREGLO[2][2]={
-{0,0}, {0,1},
-{1,0}, {1,1}
+TIPO_de_DATO NOMBRE_ARREGLO[3][2]={
+{{1,1}, {1,2}},
+{{2,1}, {2,2}},
+{{3,1}, {3,2}},
 }
 
+Es importante usar los corchetes para los arreglos, pero no indispensable.
  */
+
 #include<stdio.h>
 
 void main(){
-register  int i=0,j=0;
-float a[2][2]={1,2,3,4};
+register  int i,j;
+float a[3][2]={{1.0,5.0},{3.0,4.0},{10.0,9.0}};
 
- for(i=0;i<=1;i++)
+
+ for(i=0;i<=2;i++)
    {
      for(j=0;j<=1;j++){
-       printf("Los elementos son: %f\n",a[i][j]);
+       printf("fila %i y colu. %i. Los elementos son: %3.1f\n",i+1, j+1, a[i][j]);
    }
    }
 }
+
